3aa5265c61228b8580ed89017cacd0e77bee2a7b
lijun0222 <lijunxinghua@163.com>
2022-11-25 23:55:49 +0800

    add useMount, useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SearchPanel } from './search-panel'
 import { List } from './list'
 import { useEffect, useState } from 'react'
-import { cleanObject } from '../../utils'
+import { cleanObject, useDebounce, useMount } from 'utils'
 import qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -15,23 +15,25 @@ export const ProjectListScreen = () => {
     personId: '',
   })
 
+  const debouncedParam = useDebounce(param, 1000)
+
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(
       async (response) => {
         if (response.ok) {
           setList(await response.json())
         }
       }
     )
-  }, [param])
+  }, [debouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
   return (
     <div>
       <SearchPanel param={param} setParam={setParam} users={users} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 export const isFalsy = (value) => value === 0 ? true : !!value
 
 // 在一个函数里，改变传入的对象本身是不好的
@@ -10,4 +12,25 @@ export const cleanObject = (object) => {
     }
   })
   return result
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    // 每次在value变化后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay)
+    // 每次在上一个useEffect处理完之后再运行
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [value, delay])
+
+  return debouncedValue
 }
