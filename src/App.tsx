e5d0709d0dbde47590dcae580b7f5fac7eed4191
lijun0222 <lijunxinghua@163.com>
2022-11-25 23:11:11 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ProjectListScreen } from './screens/project-list'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ProjectListScreen />
+    </div>
+  )
+}
+
+export default App
